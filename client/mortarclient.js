bbcbd71800254e3c9768b4e3b0abffa207c3122e
Nicolas Martineau <nicolas.martineau@ingeno.ca>
2016-06-12 13:59:48 -0400

    enhancements

--- client/mortarclient.js
+++ client/mortarclient.js
@@ -1,19 +1,80 @@
-var socket;
 
-var allTanks;
+var mortarClient = {
+	socket: null,
+	players: {},
+	sandbags: [],
+	onPlayerJoinedCallback: null,
+	onPlayerLeftCallback: null,
+	onTankDestroyedCallback: null,
 
-var mortarConnect = function(host = "localhost") {
-	socket = io.connect('http://'+ host +':3000');
+	joinGame: function (host = "localhost", name, callback, callbackContext) {
+		socket = io.connect('http://'+ host +':3000');
+		socket.on('connect', function () {
+    		socket.emit('join', name, function(joinState) {
+					players = joinState.players;
+					sandbags = joinState.sandbagStates;
+					callback.call(callbackContext, joinState);
+				});
+			}
+		);
 
-  	socket.on('news', function (data) {
-    	console.log(data);
-  	});
-  	socket.on('allPositions', function (data){
-		allTanks = data;
-  	});
-}
+  		socket.on('news', function (data) {
+    		console.log(data);
+  		});
 
-var mortarUpdateMyPosition = function(x, y) {
-	socket.emit('newPosition', 
-		{ x: x, y: y });
+  		socket.on('allPlayers', function (allPlayers){
+			players = allPlayers;
+  		});
+
+  		socket.on('playerJoined', function (player){
+  			if(onPlayerJoinedCallback) {
+				onPlayerJoinedCallback.callback.call(onPlayerJoinedCallback.context, player);
+			}
+  		});
+
+	  	socket.on('playerLeft', function (playerId){  			  		
+	  		if(onPlayerLeftCallback) {
+				onPlayerLeftCallback.callback.call(onPlayerLeftCallback.context, playerId);				
+			}
+			delete players[playerId];
+	  	});
+
+  		socket.on('tankDestroyed', function (playerId){
+  			if(onTankDestroyedCallback) {
+				onTankDestroyedCallback.callback.call(onTankDestroyedCallback.context, playerId);
+			}
+  		});
+	},
+
+	onPlayerJoined: function(callback, callbackContext) {
+		onPlayerJoinedCallback = {callback: callback, context: callbackContext};
+	},
+
+	onPlayerLeft: function(callback, callbackContext) {
+		onPlayerLeftCallback = {callback: callback, context: callbackContext};
+	},
+
+	onTankDestroyed: function(callback, callbackContext) {
+		onTankDestroyedCallback = {callback: callback, context: callbackContext};
+	},
+
+	notifyState: function(state) {
+		socket.emit('newState', state);
+	},
+
+	notifyTankDestroy: function(id) {
+		socket.emit('destroy', id);
+	},
+
+	notifySandbagDestroy: function(id) {
+		socket.emit('sandbagDestroy', id);
+	},	
+
+	getPlayers: function() {
+		return players;
+	},
+
+	getSandbagStates: function() {
+		return sandbags;
+	},
 }
--- server.js
+++ server.js
@@ -4,31 +4,69 @@ var io = require('socket.io')(http);
 http.listen(3000);
 console.log("Mortar! Get down!");
 
-var tanks = [];
+var players = {};
+var sandbagStates = [];
+var playerCount = 0;
 
-io.on('connection', function(socket){
-  var socketId = socket.id;
-  console.log('New tank! : ' + socketId);
-  aNewTank = {id: socketId, x:0, y:0};
-  tanks.push(aNewTank);
+function init() {
+  for (var i = 0; i < 18; i++) {
+    sandbagStates.push(true);
+  }
+}
+
+init();
+
+io.on('connection', function(socket){  
+  socket.on('join', function(name, ack){
+    console.log('New layer! : ' + socket.id + ', ' + name);
+
+    aNewTank = {id: socket.id, playerNumber: playerCount, name: name, tankState: null};
+    players[socket.id] = aNewTank;        
+
+    var joinAckState ={ playerId: socket.id, players: players, sandbagStates : sandbagStates };    
+    playerCount++;
+
+    ack(joinAckState);
+    socket.broadcast.emit('playerJoined', aNewTank);
+  });
 
   socket.on('disconnect', function(){
     console.log('One tank left... : ' + socket.id);
-    for (var i in tanks) {
-    	if (tanks[i].id == socket.id) {
-    		tanks.splice(i,1);
+    for (var i in players) {
+    	if (players[i].id == socket.id) {
+    		delete players[i];
+    		break;
+     	}
+   	}
+
+    io.emit('playerLeft', socket.id);
+  });
+  socket.on('newState', function(state){
+  	for (var i in players) {
+    	if (players[i].id == socket.id) {
+    		players[i].tankState = state;
     		break;
      	}
    	}
   });
-  socket.on('newPosition', function(msg){
-  	for (var i in tanks) {
-    	if (tanks[i].id == socket.id) {
-    		tanks[i].x = msg.x;
-    		tanks[i].y = msg.y;
-    		break;
-     	}
-   	}
-   	io.emit('allPositions', tanks);
+  socket.on('destroy', function(id){
+    console.log('One tank Destroyed... : ' + id);
+    for (var i in players) {
+      if (players[i].id == id) {
+        delete players[i];
+        socket.broadcast.emit('tankDestroyed', id);
+        break;
+      }
+    }    
+  });
+
+  socket.on('sandbagDestroy', function(id){
+    console.log('One sandbag Destroyed... : ' + id);
+    sandbagStates[id] = false;
   });
 });
+
+// emit 20 times per second
+setInterval(function(){  
+  io.emit('allPlayers', players);
+}, 1000/60);  
